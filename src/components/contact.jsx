2a4b5ab391d31ddc6ae61be83fffb504ff713902
Tushar <tusharpatle0097@gmail.com>
2024-08-30 16:38:53 +0530

    change2

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -153,10 +153,10 @@ export const Contact = (props) => {
       <div id="footer">
         <div className="container text-center">
           <p>
-            &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
-            <a href="http://www.templatewire.com" rel="nofollow">
+            &copy; 2023 Design by TUSHAR{" "}
+            {/* <a href="http://www.templatewire.com" rel="nofollow">
               TemplateWire
-            </a>
+            </a> */}
           </p>
         </div>
       </div>
